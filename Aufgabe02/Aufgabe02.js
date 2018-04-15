032d3be505a9daf1adab4c8b5914cec76bbb0a66
Mac <Mac@192.168.8.101>
2018-04-15 15:40:26 +0200

    push A02 Test

--- Aufgabe02/Aufgabe02.js
+++ Aufgabe02/Aufgabe02.js
@@ -1,16 +1,42 @@
 var Aufgabe02;
 (function (Aufgabe02) {
+    //    Array mit Content 
     var infolist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    //    leeres Array zum Bestücken
     var cardList = [];
+    //    Prompt für Eingabe für Kartenpaare 
     function inputPairs() {
-        let pairs = prompt("Wie viele Kartenpaare (min4 & max26)");
+        let pairs = prompt("Wie viele Kartenpaare (min. 4 & max. 26)");
         let pairSum = parseInt(pairs);
-        console.log("inputPairs");
-        return pairSum;
+        if (isNaN(pairSum) || pairSum < 4 || pairSum > 26) {
+            alert("FALSCH");
+            inputPairs();
+        }
+        else {
+            console.log("inputPairs");
+            console.log(pairSum);
+            return pairSum;
+        }
+    }
+    //    Prompt für Eingabe für Spieleranzahl 
+    function inputPlayer() {
+        let n = prompt("Wieviele Spieler wollen mitspielen?(min. 1 & max. 4)");
+        let nSum = parseInt(n);
+        if (isNaN(nSum) || nSum < 0 || nSum > 4) {
+            alert("FALSCH");
+            inputPlayer();
+        }
+        else {
+            console.log("inputPlayer");
+            console.log(nSum);
+            return nSum;
+        }
     }
     let amount = inputPairs();
+    let players = inputPlayer();
     console.log("Länge Infolist", infolist.length);
     console.log("Content Infolist", infolist);
+    //    Content verdoppeln und in neuern array speichern
     function shuffelAray(x) {
         for (let i = 1; i <= x; i++) {
             var content = infolist[0];
@@ -20,6 +46,7 @@ var Aufgabe02;
         }
         console.log("Content CardList", cardList);
     }
+    //    Cards erzeugen 
     function generateCards(y) {
         var node = document.getElementById("spielfeld");
         var childNodeHTML;
@@ -45,12 +72,28 @@ var Aufgabe02;
             i++;
         }
     }
+    function generatePlayers() {
+        var node = document.getElementById("Spielerbereich");
+        var childNodeHTML;
+        var i = 0;
+        while (i < players) {
+            childNodeHTML = "<div  class='player' id='Spieler" + i + "'>";
+            childNodeHTML += "Spieler " + (i + 1);
+            childNodeHTML += "<p>";
+            childNodeHTML += "13 ";
+            childNodeHTML += "</p>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+            i++;
+        }
+    }
     // Hauptprogramm
     function main() {
         shuffelAray(amount);
         console.log("main");
         // Content erzeugen
         generateCards(amount);
+        generatePlayers();
     }
     // Add EventListener 
     document.addEventListener('DOMContentLoaded', main);
--- Aufgabe02/Aufgabe02.ts
+++ Aufgabe02/Aufgabe02.ts
@@ -1,33 +1,61 @@
 namespace Aufgabe02 {
     
 
-    
+//    Array mit Content 
     var infolist: string []=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//    leeres Array zum Bestücken
     var cardList: string[]=[];
   
     
-    
+//    Prompt für Eingabe für Kartenpaare 
    function inputPairs() : number{
        
-    let pairs : string = prompt("Wie viele Kartenpaare (min4 & max26)")
-    let pairSum : number = parseInt(pairs)
-       
+    let pairs : string = prompt("Wie viele Kartenpaare (min. 4 & max. 26)");
+    let pairSum : number = parseInt(pairs);
+       if (isNaN(pairSum) || pairSum < 4 || pairSum > 26) {
+        alert("FALSCH")
+        inputPairs();    
+    }
+    
+        else {
        
       
        console.log("inputPairs")
         
-    
+    console.log(pairSum);
        return pairSum;
+           
     }
+       }
+    
+    //    Prompt für Eingabe für Spieleranzahl 
+
+       function inputPlayer():number{
+           let n : string = prompt ("Wieviele Spieler wollen mitspielen?(min. 1 & max. 4)");
+           let nSum : number = parseInt(n);
+            if (isNaN(nSum) || nSum < 0 || nSum > 4) {
+        alert("FALSCH")
+        inputPlayer();    
+    }
+    
+        else {
        
+      
+       console.log("inputPlayer")
+        
+    console.log(nSum);
+       return nSum;
+           
+    }
+     }      
    let amount :number =  inputPairs();
-   
+   let players : number = inputPlayer();
         
     console.log("Länge Infolist", infolist.length);
     console.log("Content Infolist", infolist);
     
     
-    
+//    Content verdoppeln und in neuern array speichern
     
     function shuffelAray(x:number): void {
         
@@ -48,7 +76,7 @@ namespace Aufgabe02 {
     
     }
     
-    
+//    Cards erzeugen 
     
     function generateCards(y:number) : void{
         var node : any= document.getElementById("spielfeld");
@@ -80,7 +108,22 @@ namespace Aufgabe02 {
             i++;
         }
     }
-    
+    function generatePlayers() : void{
+        var node : any= document.getElementById("Spielerbereich");
+        var childNodeHTML : string;
+        var i : number = 0;
+        while (i < players) {
+          
+            childNodeHTML = "<div  class='player' id='Spieler" + i + "'>";
+            childNodeHTML += "Spieler " + (i+1);
+            childNodeHTML += "<p>";
+            childNodeHTML += "13 ";
+            childNodeHTML += "</p>";
+            childNodeHTML += " </div> ";      
+            node.innerHTML += childNodeHTML;
+            i++;
+        }
+        }
     
     // Hauptprogramm
     function main () : void {
@@ -90,11 +133,12 @@ namespace Aufgabe02 {
        
         
         
-        console.log("main");
+         console.log("main");
         
         // Content erzeugen
          generateCards(amount);
-        }
+         generatePlayers();
+         }
     
     
     
